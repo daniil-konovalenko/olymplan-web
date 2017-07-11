c60741b96f3acb240ff4b0a37c550f48b4f81b18
Anata Imoto <anibaka9@gmail.com>
2017-07-11 15:04:08 +0300

    small fix

--- routes/contest.js
+++ routes/contest.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-//var uniShort = require('./uni_short');
-//router.use('/uni/:short', uniShort);
+var uniShort = require('./uni_short');
+router.use('/', uniShort);
 
 router.get('/', (req, res, next) => {
     res.send('contest page yo!');
--- routes/uni_short.js
+++ routes/uni_short.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var UniShortFac = require('./uni_short_fac');
+
+router.use('/', UniShortFac)
+
 router.get('/:short', (req, res, next) => {
     res.send(req.params.short);
 });
