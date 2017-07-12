91294c158e96e414622d4cf9aad4ebc0edeb4f54
Daniel Konovalenko <ddkono@gmail.com>
2017-07-12 17:06:41 +0300

    Add Stage model except for relation with Contest

--- models/stage.js
+++ models/stage.js
@@ -1,26 +1,26 @@
 'use strict';
-
+var Contest = require('./contest');
 
 module.exports = function(sequelize, DataTypes) {
-  var Stage = sequelize.define('Stage', {
-    places: DataTypes.ARRAY(DataTypes.STRING),
-    date: DataTypes.DATE,
-    isIntramural: DataTypes.BOOLEAN,
-    startTime: DataTypes.DATE,
-    duration: DataTypes.TIME,
-    deadline: DataTypes.DATE,
-    notes: DataTypes.TEXT
+    var Stage = sequelize.define('Stage', {
+        places: DataTypes.ARRAY(DataTypes.STRING),
+        date: DataTypes.DATE,
+        isIntramural: DataTypes.BOOLEAN,
+        startTime: DataTypes.DATE,
+        duration: DataTypes.TIME,
+        deadline: DataTypes.DATE,
+        notes: DataTypes.TEXT
 
-  }, {
-    classMethods: {
-      associate: function(models) {
-
-      }
-    }
-  });
-  Stage.hasMany(Stage, {as: 'requiredStages'});
-  return Stage;
+    }, {
+        classMethods: {
+            associate: function(models) {
+            }
+        }
+    });
+    Stage.hasMany(Stage, {as: 'requiredStages'});
+    Stage.belongsToMany(Contest, {
+      through: 'ContestStages',
+      as: 'contests'
+    });
+    return Stage;
 };
-
-
-
