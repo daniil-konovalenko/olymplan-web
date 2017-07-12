95d1ebefe51b215af0a34216075b9b7a5c6257e4
Aleksey Budkin <anibaka9@gmmail.com>
2017-07-12 14:35:58 +0300

    add some tests for pages

--- test/server.js
+++ test/server.js
@@ -5,11 +5,36 @@ describe('loading express', function () {
     beforeEach(function () {
         app = require('../app');
     });
-    
+
 
     it('responds to /', function testRoot(done){
         request(app)
             .get('/')
+            .expect(200, done);
+    });
+    it('responds to /api', function testRoot(done){
+        request(app)
+            .get('/api')
+            .expect(200, done);
+    });
+    it('responds to /adminmyass', function testRoot(done){
+        request(app)
+            .get('/adminmyass')
+            .expect(200, done);
+    });
+    it('responds to /contest', function testRoot(done){
+        request(app)
+            .get('/contest')
+            .expect(200, done);
+    });
+    it('responds to /uni', function testRoot(done){
+        request(app)
+            .get('/uni')
+            .expect(200, done);
+    });
+    it('responds to /uni/some', function testRoot(done){
+        request(app)
+            .get('/uni/some')
             .expect(200, done);
     });
     it('404 some random page', function testRandomPage(done){
