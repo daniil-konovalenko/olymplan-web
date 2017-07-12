f7004669d2b2ff35f2353230e369a4d63e7dca21
Andrey Sokolov <sokolov-as-i@yandex.ru>
2017-07-12 14:38:16 +0300

    extend uitest

--- routes/uitest.js
+++ routes/uitest.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('Heeeeey... Seems like u r goin to test something!');
+    res.render("uitest");
 });
 
 module.exports = router;
