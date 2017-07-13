7ee8edee407d21c31f00527d4131d39c694a4f6f
Daniel Konovalenko <ddkono@gmail.com>
2017-07-13 12:47:20 +0300

    Move all uni paths to one file

--- routes/uni.js
+++ routes/uni.js
@@ -2,11 +2,21 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models/index');
 var uniContoller = require('../controllers/universityController');
-var uniShort = require('./uni_short');
 
-router.use('/', uniShort);
 
-router.get('/', uniContoller.university_list);
+router.get('/', (req, res, next) => {
+    res.send('uni')
+});
+
+router.get('/all', uniContoller.university_list);
+
+router.get('/:short', (req, res, next) => {
+    res.send(req.params.short);
+});
+
+router.get('/:short/:fac', (req, res, next) => {
+    res.send(req.params.short + ': ' + req.params.fac);
+});
 
 
 module.exports = router;
