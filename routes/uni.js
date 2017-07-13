0d7afa03468be3a4ffa46aaaa7eec0e85c7ed0e9
topshik <aasemenkin@edu.hse.ru>
2017-07-13 11:57:04 +0300

    uni page has data from data

--- routes/uni.js
+++ routes/uni.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models/index');
 
 var uniShort = require('./uni_short');
 
 router.use('/', uniShort);
 
-router.get('/', (req, res, next) => {
-    res.send('uni yo!');
+router.get('/', /* async */ (req, res, next) => {
+    models.University.findAll({}).then(function(todos) {
+        res.json(todos);
+    });
+    /*
+
+University.findOne({where: }).then((data) =>  {
+    res.send(data);
+})
+
+var data = await University.findOne({where:});
+JSON.stringify(data);
+
+    */
+
 });
 
 module.exports = router;
