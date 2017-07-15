74dffebb89b1fa7ca62819f42cf33fc71874a130
topshik <aasemenkin@edu.hse.ru>
2017-07-15 17:11:56 +0300

    style issues

--- models/university.js
+++ models/university.js
@@ -12,7 +12,7 @@ module.exports = function(sequelize, DataTypes) {
     }, {
         classMethods: {
             associate: function(models) {
-                University.hasMany(models.Faculty)
+                University.hasMany(models.Faculty);
             }
         },
     });
