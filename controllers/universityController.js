4d2d77878d3d12bec2d5477b08c2317de71c6a89
Daniel Konovalenko <ddkono@gmail.com>
2017-07-15 15:41:34 +0300

    Add Faculties to University request in University controller. Add Faculties enumeration to uni/ page

--- controllers/universityController.js
+++ controllers/universityController.js
@@ -1,13 +1,14 @@
 var models = require('../models');
 
 exports.universityList = async (req, res, next) => {
-    var unis = await models.University.findAll();
+    var unis = await models.University.findAll({include: [ models.Faculty ]});
     res.render('unis', {title: 'Университеты', unis:unis});
 };
 
 exports.universityByName = async (req, res, next) => {
     var uni = await models.University.findOne({
         where: {name: req.params.short},
+        include: [ models.Faculty ],
     });
     res.render('uni_page', {title: uni.name, uni:uni});
 };
@@ -15,6 +16,7 @@ exports.universityByName = async (req, res, next) => {
 exports.facultyByName = async (req, res, next) => {
     var fac = await models.Faculty.findOne({
         where: {name: req.params.fac},
+        include: [ models.Faculty ],
     });
     res.render('fac_page', {title: fac.name, fac:fac});
 };
