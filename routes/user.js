fe7f74e63ca79ad5d8aa4caa04435d2c904d3ff9
Andrey Sokolov <sokolov-as-i@yandex.ru>
2017-07-15 13:19:34 +0300

    add register steps

--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,12 @@ router.get('/', (req, res, next) => {
     res.render("user/settings");
 });
 
+router.get('/step1', (req, res, next) => {
+    res.render("user/register-step1");
+});
+
+router.get('/step2', (req, res, next) => {
+    res.render("user/register-step2");
+});
+
 module.exports = router;
