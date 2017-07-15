951c62c275780c64f1e2053b0a6fb20bfafb577b
Andrey Sokolov <sokolov-as-i@yandex.ru>
2017-07-15 19:41:32 +0300

    try to do admin/index

--- routes/adminmyass.js
+++ routes/adminmyass.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
-router.get('/', (req, res, next) => {
-    res.render("admin/index");
+router.get('/', async (req, res, next) => {
+    var unis = await models.University.findAll();
+    var facs = await models.Faculty.findAll();
+
+    console.log(JSON.stringify(unis));
+
+    res.render("admin/index", {
+        unis: unis,
+        facs: facs
+    });
 });
 
 module.exports = router;
