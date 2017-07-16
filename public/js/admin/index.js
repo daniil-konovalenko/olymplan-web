47f45f60e46e330342445262f3fcfceca7eb8190
Andrey Sokolov <sokolov-as-i@yandex.ru>
2017-07-16 16:09:21 +0300

    almost working save button

--- public/js/admin/index.js
+++ public/js/admin/index.js
@@ -4,4 +4,18 @@ $(document).ready(function(){
         $(this).parent().children(".elem__body").toggleClass("_is_hidden");
         console.log($(this).parent().children(".elem__body"));
     });
+
+    $(".button__submit").click(function() {
+        //console.log($(this));
+        var pid = $(this).attr('id');
+        //alert(pid);
+        var data = {};
+
+        console.log(fields);
+
+        for (var field in fields) {
+            console.log(field);
+            console.log($(".-value-" + pid + "-" + field).text());
+        }
+    });
 });
