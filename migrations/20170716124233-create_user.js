adc942e79f1a97e5e249993326e99206e191cb4e
Daniel Konovalenko <ddkono@gmail.com>
2017-07-16 15:48:23 +0300

    Add empty migrations

---
+++ migrations/20170716124233-create_user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+    up: function (queryInterface, Sequelize) {
+        /*
+         Add altering commands here.
+         Return a promise to correctly handle asynchronicity.
+
+         Example:
+         return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
+    },
+
+    down: function (queryInterface, Sequelize) {
+        return queryInterface.dropTable('Users');
+    }
+};
