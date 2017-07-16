55f950e161334ea47ba9d971036b683ea176e4e8
Daniel Konovalenko <ddkono@gmail.com>
2017-07-16 15:55:41 +0300

    Add Bonus create migration

--- migrations/20170716124142-create_bonus.js
+++ migrations/20170716124142-create_bonus.js
@@ -2,13 +2,41 @@
 
 module.exports = {
     up: function (queryInterface, Sequelize) {
-        /*
-         Add altering commands here.
-         Return a promise to correctly handle asynchronicity.
-
-         Example:
-         return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-         */
+        return queryInterface.createTable('Bonus', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            diploma_level:{
+                type: Sequelize.Integer,
+            },
+            subject:{
+                type: Sequelize.String,
+            },
+            bonus_type: {
+                type: Sequelize.String
+            },
+            ContestId: {
+                type: Sequelize.INTEGER,
+                onDelete: "CASCADE",
+                allowNull: false,
+                references: {
+                    model: 'Contests',
+                    key: 'id'
+                }
+            },
+            EduProgramId: {
+                type: Sequelize.INTEGER,
+                onDelete: "CASCADE",
+                allowNull: false,
+                references: {
+                    model: 'EduPrograms',
+                    key: 'id'
+                }
+            },
+        });
     },
 
     down: function (queryInterface, Sequelize) {
