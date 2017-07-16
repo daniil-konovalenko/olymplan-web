fef2d8535b0515e98237a6a4750d6b8256246f7b
Daniel Konovalenko <ddkono@gmail.com>
2017-07-16 16:36:23 +0300

    Add Stage create migration, fix field naming

--- migrations/20170716124222-create_stage.js
+++ migrations/20170716124222-create_stage.js
@@ -2,13 +2,44 @@
 
 module.exports = {
     up: function (queryInterface, Sequelize) {
-        /*
-         Add altering commands here.
-         Return a promise to correctly handle asynchronicity.
-
-         Example:
-         return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-         */
+        return queryInterface.createTable('Contests', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            places: {
+                type: Sequelize.ARRAY(Sequelize.STRING),
+            },
+            date: {
+                type: Sequelize.DATE,
+            },
+            is_intramural: {
+                type: Sequelize.BOOLEAN,
+            },
+            start_time: {
+                type: Sequelize.DATE,
+            },
+            duration: {
+                type: Sequelize.TIME,
+            },
+            deadline: {
+                type: Sequelize.DATE,
+            },
+            notes: {
+                type: Sequelize.TEXT,
+            },
+            StageId: {
+                type: Sequelize.INTEGER,
+                onDelete: "CASCADE",
+                allowNull: true,
+                references: {
+                    model: 'Stage',
+                    key: 'id'
+                }
+            },
+        });
     },
 
     down: function (queryInterface, Sequelize) {
